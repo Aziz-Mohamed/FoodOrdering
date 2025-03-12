be949733030c2a457eefb4cf809620ad87436a09
AbdilAziz <abdilaziz.m.elsayed@gmail.com>
2025-03-12 14:24:32 +0300

    feat: establish admin side

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -21,19 +21,25 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: Colors.light.background,
+        tabBarInactiveTintColor: 'gainsboro',
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: Colors.light.tint,
+        },
       }}
     >
-      <Tabs.Screen name="index" options={{href:null}} />
+      <Tabs.Screen name="index" options={{ href: null }} />
       <Tabs.Screen
         name="menu"
         options={{
           title: "Menu",
           headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="cutlery" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="cutlery" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
